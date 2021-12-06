18afd020699855019799c9989ed600272fcf6b3e
franciscomarin98 <francmarinc@gmail.com>
2021-12-05 23:59:04 -0500

    Cleanup

---
+++ src/HeroeApp.tsx
@@ -0,0 +1,9 @@
+const HeroeApp = () => {
+    return (
+        <div className={'container'}>
+            <h1>Heroes App</h1>
+        </div>
+    );
+};
+
+export default HeroeApp;
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+
+import HeroeApp from './HeroeApp'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HeroeApp />
   </React.StrictMode>,
   document.getElementById('root')
 )
